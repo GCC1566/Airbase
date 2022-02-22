f9ccd5dc21e8b3fac4f79cb5329ba965b6c2c796
gcc1566 <156654540@qq.com>
2022-02-22 17:41:28 +0800

    new code

--- src/main/java/com/gcc/airbase/StartApplication.java
+++ src/main/java/com/gcc/airbase/StartApplication.java
@@ -2,9 +2,10 @@ package com.gcc.airbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StartApplication {
 
     public static void main(String[] args){
